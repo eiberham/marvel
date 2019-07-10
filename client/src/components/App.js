824534f290d9bfc99421581892913eaadecae8f1
Abraham Cedeno <acedeno@zetech.com.ar>
2019-07-09 23:41:10 -0300

    client boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CharactersList from './CharactersList'
+
+const App = () => {
+    return (
+        <div>
+        
+        App
+            <CharactersList />
+        </div>
+    );
+};
+
+
+export default App;
---
+++ client/src/components/CharactersList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import marvel from "../apis/marvel";
+
+const CharactersList = () => {
+    return (
+        <div>Characters</div>
+    );
+};
+
+export default CharactersList;
